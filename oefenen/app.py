6ad9c3e618705cd216e8b7e1602f210e43c745b1
FabioSalis <9019469@student.zadkine.nl>
2022-09-30 15:44:18 +0200

    commit python

--- oefenen/app.py
+++ oefenen/app.py
@@ -1,4 +1,5 @@
-first_name = "Fabio"
-Last_name = "Salis"
+first_name = input("First Name:")
+Last_name = input ("Last name:")
 Fullname = first_name + " " + Last_name
 welcome_message = "welcome " + Fullname + "!!!"
+print(welcome_message)
